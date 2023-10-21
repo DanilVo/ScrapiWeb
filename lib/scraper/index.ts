2dd71b4a294057d6c485ff0cbfb234dc6886fd17
danil volobuyev <dani736@gmail.com>
2023-10-21 14:14:47 +0300

    fix mailer

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,11 +1,12 @@
-import axios from "axios";
-import * as cheerio from "cheerio";
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import { extractCurrency, extractDescription, extractPrice } from '../utils';
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
 
-  const username = "brd-customer-hl_e91e4567-zone-scrapiweb";
-  const password = "bpcmd6t13vil";
+  const username = 'brd-customer-hl_e91e4567-zone-scrapiweb';
+  const password = 'bpcmd6t13vil';
   const port = 22225;
   const session_id = (1000000 * Math.random()) | 0;
 
@@ -14,18 +15,67 @@ export async function scrapeAmazonProduct(url: string) {
       username: `${username}-session-${session_id}`,
       password,
     },
-    host: "brd.superproxy.io",
+    host: 'brd.superproxy.io',
     port,
     rejectUnauthorized: false,
   };
- 
+
   try {
     // Fetch the product page
     const response = await axios.get(url, options);
     const $ = cheerio.load(response.data);
-
     const title = $(`#productTitle`).text().trim();
-    console.log(title);
+    const currentPrice = extractPrice(
+      $('.priceToPay span.a-price-whole'),
+      $('a.size.base.a-color-price'),
+      $('.a-button-selected .a-color-base')
+    );
+    const originalPrice = extractPrice(
+      $('#priceblock_ourprice'),
+      $('.a-price.a-text-price span.a-offscreen'),
+      $('#listPrice'),
+      $('#priceblock_dealprice'),
+      $('.a-size-base.a-color-price')
+    );
+    const outOfStock =
+      $('#availability span').text().trim().toLowerCase() ===
+      'currently unavailable';
+
+    const image =
+      $('#imgBlkFront').attr('data-a-dynamic-image') ||
+      $('#landingImage').attr('data-a-dynamic-image') ||
+      '{}';
+    const imageUrls = Object.keys(JSON.parse(image));
+
+    const currency = extractCurrency($('.a-price-symbol'));
+
+    const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, '');
+
+    const description = extractDescription($);
+
+    // Construct data object with scrapped information
+
+    const data = {
+      url,
+      currency: currency || '$',
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: 'category', // future implementation
+      reviewsCount: 0, // future implementation
+      starts: 0, // future implementation
+      isOutOfStock: outOfStock,
+      description,
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      average: Number(currentPrice) || Number(originalPrice),
+    };
+
+    return data;
+    
   } catch (error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`);
   }
